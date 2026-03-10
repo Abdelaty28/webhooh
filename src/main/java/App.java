79d0fb62ae706a23d48bf173fe66dfd3e746b9ca
Mahmoud Abdelaty <asd790635@gmail.com>
2026-03-11 01:40:49 +0200

    Update Jenkins greeting message in App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello from Jenkins build test 🚀");
+        System.out.println("Hello from Jenkins  🚀");
 
         System.out.println("Second webhook test from Mahmoud");
 
