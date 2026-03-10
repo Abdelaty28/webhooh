748e0afe99557662112a559715a12bcbd3378c1f
Mahmoud Abdelaty <asd790635@gmail.com>
2026-03-10 05:37:37 +0200

    another edit to use webhook

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,11 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Auto Build 🚀");
+
+        System.out.println("Hello from Jenkins build test 🚀");
+
+        System.out.println("Second webhook test from Mahmoud");
+
     }
 
 }
